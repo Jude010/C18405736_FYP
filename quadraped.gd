3e00ea6048653aef19cf1685bcae7a642aaa3de9
Jude Lysaght <lysaghtjude@gmail.com>
2026-03-10 17:01:04 +0000

    spring

--- quadraped.gd
+++ quadraped.gd
@@ -1,1 +1,7 @@
 extends Node3D
+
+func _process(delta: float) -> void:
+	$Armature/Skeleton3D/FABRIK3D/FrontLeft_target.position = $SpringArmFrontLeft/Front_left_spring.position
+	$Armature/Skeleton3D/FABRIK3D/FrontRight_target.position = $SpringArmFrontRight/Front_right_spring.position
+	$Armature/Skeleton3D/FABRIK3D/BackLeft_target.position = $SpringArmBackLeft/Back_left_spring.position
+	$Armature/Skeleton3D/FABRIK3D/BackRight_target.position = $SpringArmBackRight/Back_right_spring.position
