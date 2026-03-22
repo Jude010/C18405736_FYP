6e30d92da04f92d7dc53a4ca27b56cafb8faefc2
jude010 <lysaghtjude@gmail.com>
2026-03-22 21:20:11 +0000

    small refactor

--- quadraped.gd
+++ quadraped.gd
@@ -1,7 +1,45 @@
 extends Node3D
 
-func _process(delta: float) -> void:
-	$Armature/Skeleton3D/FABRIK3D/FrontLeft_target.position = $SpringArmFrontLeft/Front_left_spring.position
-	$Armature/Skeleton3D/FABRIK3D/FrontRight_target.position = $SpringArmFrontRight/Front_right_spring.position
-	$Armature/Skeleton3D/FABRIK3D/BackLeft_target.position = $SpringArmBackLeft/Back_left_spring.position
-	$Armature/Skeleton3D/FABRIK3D/BackRight_target.position = $SpringArmBackRight/Back_right_spring.position
+@export var speed:float = 100
+@export var speed_r:int = 3
+
+func _process(_delta: float) -> void:
+	$Body/quadraped/Armature/Skeleton3D/FABRIK3D/FrontLeft_target.position = $springs/SpringArmFrontLeft/Front_left_spring.position
+	$Body/quadraped/Armature/Skeleton3D/FABRIK3D/FrontRight_target.position = $springs/SpringArmFrontRight/Front_right_spring.position
+	$Body/quadraped/Armature/Skeleton3D/FABRIK3D/BackLeft_target.position = $springs/SpringArmBackLeft/Back_left_spring.position
+	$Body/quadraped/Armature/Skeleton3D/FABRIK3D/BackRight_target.position = $springs/SpringArmBackRight/Back_right_spring.position
+
+func _physics_process(delta: float) -> void:
+	var dir:Vector3 = Vector3.ZERO
+	var rot:int = 0
+	var body:CharacterBody3D = $Body
+	
+	if Input.is_action_pressed("forward"):
+		dir = Vector3.FORWARD + dir
+		
+	if Input.is_action_pressed("back"):
+		dir = Vector3.BACK + dir
+		
+	if Input.is_action_pressed("left"):
+		dir = dir + Vector3.LEFT
+	
+	if Input.is_action_pressed("right"):
+		dir = dir + Vector3.RIGHT
+	
+	if Input.is_action_pressed("rot_clockwise"):
+		rot = rot + 1 
+	
+	if Input.is_action_pressed("rot_anti_clockwise"):
+		rot = rot - 1
+	
+	if Input.is_action_pressed("shift"):
+		dir = dir + Vector3.DOWN
+
+	if Input.is_action_pressed("space"):
+		dir = dir + Vector3.UP
+	
+	body.rotate_y(speed_r * rot * delta)
+	
+	body.velocity = body.global_transform.basis * dir.normalized() * speed * delta
+	
+	body.move_and_slide()
